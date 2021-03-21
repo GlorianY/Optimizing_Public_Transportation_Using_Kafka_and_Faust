206b07b64834491756b6a26706e3081fe9a56c41
GlorianY <daniel.glorian20@gmail.com>
2021-03-21 01:20:01 +0100

    add initial codes

---
+++ README.md
@@ -0,0 +1,4 @@
+# Optimizing Public Transportation Using Kafka and Faust
+This repository contains my code in attempting to tackle the Udacity Data Streaming Nanodegree program.
+
+The goal of the task is to develop a Kafka producer which emits system status for commuters in a streaming fashion. Another goal is, to develop a Faust-based consumer to consume the streamed commuter system status data.
---
+++ producers/models/turnstile.py
@@ -0,0 +1,57 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        super().__init__(
+            "chicago_turnstile", # topic name cannot be station specific, since it may break ksql.py
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema, 
+            num_partitions=1,
+            num_replicas=1,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+        # make sure you check the turnstile topic by
+        # running ksql and show_topics;
+        # If you change this topic name, you may need to
+        # re-run the producer and faust
+        self.topic_name = f'chicago_turnstile'
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={"station_id":self.station.station_id,
+                   "station_name":self.station.name,
+                   "line":self.station.color.name
+            },
+        )
